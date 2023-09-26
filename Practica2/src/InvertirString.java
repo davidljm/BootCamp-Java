1064b9e2f328dabbfd0c0f1c2971c92648802f43
David Mulé <davidljmule@hotmail.com>
2023-09-25 22:33:19 -0300

    se agrega erjercicio invertir string

---
+++ Practica2/src/InvertirString.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class InvertirString {
+    // Método recursivo para invertir un String
+    public String invertirString(String str) {
+        // Caso base: Si la cadena está vacía o tiene un solo carácter, devolvemos la cadena original.
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+
+        // Paso recursivo: Tomamos el primer carácter de la cadena y lo concatenamos
+        // con la inversión del resto de la cadena (todos los caracteres excepto el primero).
+        char primerCaracter = str.charAt(0);
+        String restoDeCadena = str.substring(1);
+
+        return invertirString(restoDeCadena) + primerCaracter;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una cadena para invertir: ");
+        String cadena = scanner.nextLine();
+
+        InvertirString inversor = new InvertirString();
+        String cadenaInvertida = inversor.invertirString(cadena);
+
+        System.out.println("Cadena original: " + cadena);
+        System.out.println("Cadena invertida: " + cadenaInvertida);
+
+        scanner.close();
+    }
+}
